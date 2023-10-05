da38395dd1d998225582c0735fd800eb5de239c9
Vinod Vaman Bhat <vinodvamanbhat@gmail.com>
2023-10-05 12:56:39 +0530

    Homepage almost done

--- connection/databaseCRUD.py
+++ connection/databaseCRUD.py
@@ -27,9 +27,12 @@ class DatabaseObject:
     def insertOne(self, data):
         self.DATABASE['Food'].insert_one(data)
 
-    def countDocuments(self):
-        count = self.DATABASE['Food'].count_documents({})
-        return count
+    def getFoodTitles(self):
+        food_titles = []
+        cursor = list(self.DATABASE['Food'].find({}, {"_id": 0, "ingredients": 0, "directions": 0, "link": 0, "narration": 0, "image_url": 0 }))
+        for document in cursor:
+            food_titles.append(document)
+        return food_titles
     
     def findRecommendedFood(self, food):
         food_data = []
@@ -48,7 +51,7 @@ class DatabaseObject:
     def displayData(self, pageNo):
         data = []
         no_of_data = 20
-        cursor = list(self.DATABASE['Food'].find({}, {"_id": 0, "ingredients": 0, "directions": 0, "link": 0, "narration": 0}).skip(no_of_data * pageNo).limit(no_of_data))
+        cursor = list(self.DATABASE['Food'].find({}, {"_id": 0, "directions": 0, "link": 0, "narration": 0}).skip(no_of_data * pageNo).limit(no_of_data))
         for document in cursor:
             data.append(document)
         return data
